dde9d4937d33b55ce1ba6b127ca7b45ddf1d9ec5
Badre-s <bsahili@hotmail.fr>
2024-10-23 01:16:29 +0200

    Update PokemonCard.tsx

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,7 +1,11 @@
 function PokemonCard({ pokemon }) {
 	return (
 		<figure>
-			<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>
+			{pokemon.imgSrc ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			) : (
+				<p>???</p>
+			)}
 			<figcaption>{pokemon.name}</figcaption>
 		</figure>
 	);
