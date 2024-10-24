8ba7cec2422ebd8e12c9406021f5cad12261275a
Badre-s <bsahili@hotmail.fr>
2024-10-24 21:19:39 +0200

    rendering-list.1

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,29 +16,33 @@ const NavBar = ({
 	setPokemonIndex,
 	pokemonList,
 }: NavBarProps) => {
-	const handlePrecedentClick = () => {
-		setPokemonIndex(pokemonIndex - 1);
-	};
-	const handleSuivantClick = () => {
-		setPokemonIndex(pokemonIndex + 1);
-	};
-
 	return (
-		<div>
-			<PokemonCard pokemon={pokemonList[pokemonIndex]} />
-			{pokemonIndex > 0 && (
-				<button type="button" onClick={handlePrecedentClick}>
-					Precedant
-				</button>
-			)}
-			{pokemonIndex < pokemonList.length - 1 && (
-				<button type="button" onClick={handleSuivantClick}>
-					Suivant
-				</button>
-			)}
-		</div>
+		<section>
+			<div>
+				<PokemonCard pokemon={pokemonList[pokemonIndex]} />
+				{pokemonList.map((pokemon, index) => (
+					<span key={pokemon.name}>
+						<button type="button" onClick={() => setPokemonIndex(index)}>
+							{" "}
+							{pokemon.name}{" "}
+						</button>
+					</span>
+				))}
+			</div>
+		</section>
 	);
 };
+
 export default NavBar;
 
-// ༼ つ ◕_◕ ༽つ // ༼ つ ◕_◕ ༽つ // ༼ つ ◕_◕ ༽つ // ༼ つ ◕_◕ ༽つ // ༼ つ ◕_◕ ༽つ // ༼ つ ◕_◕ ༽つ
+//       ⢀⡞⠳⣄⣀⣀⣀⣠⠞⢷⡀
+//      ⣸⠟⠋⠁      ⠈⠙⠻⣇    ⣀
+//     ⡾⠁⢰⣿⡆  ⣀  ⢰⣽⡆⠈⢧⢰⠛⡇
+//    ⣸⢷⠶⠒⠉  ⠒⠭⠒  ⠉⠒⢶⠾⣏⡼⠃
+//    ⣿⠶⣤⣀⣀        ⣀⣀⣤⠶⡟⠁
+//    ⢸    ⠈⠉⠙⠛⠛⠉⠉⠁    ⣏⣉⡙⣆
+//    ⠘⡆                 ⢸⠃  ⢹⢸
+//     ⣇                 ⢸⢀⡴⢣⠇
+//     ⢸                 ⣸⠭⠔⠁
+//     ⠸⣆⣀            ⣀⣰⠇
+//         ⠉⠙⠓⠒⠒⠒⠚⠋⠉
